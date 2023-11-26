13f440c9e87738895d8de58f9d859f887cf6dda7
Yadhun Sajith V V <yadhunsajith2002@gmail.com>
2023-11-26 15:41:20 +0530

    search screen updated

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:todo_app/controller/home_controller/home_controller.dart';
 import 'package:todo_app/controller/note_controller/note_controller.dart';
+import 'package:todo_app/controller/search_controller/search_controller.dart';
 import 'package:todo_app/model/event_model.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_app/view/splash_screen/splash_screen.dart';
@@ -20,6 +21,7 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(create: (context) => NoteController()),
         ChangeNotifierProvider(create: (context) => HomeController()),
+        ChangeNotifierProvider(create: (context) => SearchService())
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
--- lib/view/search_screen/search_screen.dart
+++ lib/view/search_screen/search_screen.dart
@@ -1,17 +1,25 @@
 // import 'package:flutter/material.dart';
-// import 'package:hive_flutter/hive_flutter.dart';
+
 // import 'package:provider/provider.dart';
 // import 'package:todo_app/controller/note_controller/note_controller.dart';
-// import 'package:todo_app/model/event_model.dart';
+// import 'package:todo_app/controller/search_controller/search_controller.dart';
+// import 'package:lottie/lottie.dart';
+// import 'package:todo_app/view/description_screen.dart/description_screen.dart';
 
-// class SearchScreen extends StatelessWidget {
-//   SearchScreen({super.key});
+// class SearchScreen extends StatefulWidget {
+//   SearchScreen({Key? key}) : super(key: key);
 
+//   @override
+//   _SearchScreenState createState() => _SearchScreenState();
+// }
+
+// class _SearchScreenState extends State<SearchScreen> {
 //   final TextEditingController searchController = TextEditingController();
 
 //   @override
 //   Widget build(BuildContext context) {
 //     var noteController = context.read<NoteController>();
+//     var searchProvider = context.read<SearchService>();
 
 //     return Scaffold(
 //       backgroundColor: Colors.black,
@@ -25,11 +33,21 @@
 //               controller: searchController,
 //               onChanged: (value) {
 //                 // Filter notes based on the search query
+//                 if (value.isEmpty) {
+//                   // If the search query is empty, show recent searches
+//                   setState(() {});
+//                 }
 //                 noteController.filterNotes(value);
+//                 setState(() {});
 //               },
 //               decoration: InputDecoration(
 //                 hintText: "Search for Notes",
-//                 prefixIcon: Icon(Icons.search),
+//                 suffixIcon: InkWell(
+//                     onTap: () {
+//                       noteController.filterNotes(searchController.text);
+//                       setState(() {});
+//                     },
+//                     child: Icon(Icons.search)),
 //                 border: OutlineInputBorder(borderSide: BorderSide.none),
 //               ),
 //             ),
@@ -38,39 +56,135 @@
 //       ),
 //       body: Consumer<NoteController>(
 //         builder: (context, noteController, _) {
-//           return Column(
-//             children: [
-//               Expanded(
-//                 child: ListView.builder(
-//                   itemCount: noteController.notes.length,
-//                   itemBuilder: (context, index) {
-//                     final note = noteController.notes[index];
-//                     return Card(
-//                       child: Center(
-//                         child: Padding(
-//                           padding: const EdgeInsets.all(15.0),
-//                           child: Text(
-//                             note.title,
-//                             style: TextStyle(color: Colors.white),
+//           if (searchController.text.isEmpty) {
+//             // Show recent searches when the search query is empty
+//             return FutureBuilder<List<String>>(
+//               future: searchProvider.getRecentSearches(),
+//               builder: (context, snapshot) {
+//                 if (snapshot.connectionState == ConnectionState.waiting) {
+//                   return CircularProgressIndicator();
+//                 } else if (snapshot.hasError) {
+//                   return Text('Error: ${snapshot.error}');
+//                 } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+//                   return Center(
+//                     child: Text(
+//                       'No recent searches',
+//                       style: TextStyle(
+//                         color: Colors.grey,
+//                         fontSize: 20,
+//                         fontWeight: FontWeight.w400,
+//                       ),
+//                     ),
+//                   );
+//                 } else {
+//                   return Padding(
+//                     padding: const EdgeInsets.symmetric(horizontal: 20),
+//                     child: Column(
+//                       crossAxisAlignment: CrossAxisAlignment.start,
+//                       children: [
+//                         SizedBox(
+//                           height: 15,
+//                         ),
+//                         Text(
+//                           "Recent Searches",
+//                           style: TextStyle(fontSize: 18),
+//                         ),
+//                         SizedBox(
+//                           height: 15,
+//                         ),
+//                         Expanded(
+//                           child: ListView.builder(
+//                             scrollDirection: Axis.vertical,
+//                             itemCount: snapshot.data!.length,
+//                             itemBuilder: (context, index) {
+//                               final recentSearch = snapshot.data![index];
+//                               return Padding(
+//                                 padding:
+//                                     const EdgeInsets.symmetric(horizontal: 8.0),
+//                                 child: InkWell(
+//                                   onTap: () {
+//                                     searchController.text = recentSearch;
+//                                   },
+//                                   child: Row(
+//                                     mainAxisAlignment:
+//                                         MainAxisAlignment.spaceBetween,
+//                                     children: [
+//                                       Text(
+//                                         recentSearch,
+//                                         style: TextStyle(
+//                                             fontSize: 16, color: Colors.grey),
+//                                       ),
+//                                       IconButton(
+//                                         icon: Icon(Icons.close),
+//                                         onPressed: () async {
+//                                           await searchProvider
+//                                               .removeRecentSearch(recentSearch);
+//                                           setState(() {});
+//                                         },
+//                                       ),
+//                                     ],
+//                                   ),
+//                                 ),
+//                               );
+//                             },
 //                           ),
 //                         ),
-//                       ),
-//                     );
-//                   },
+//                       ],
+//                     ),
+//                   );
+//                 }
+//               },
+//             );
+//           } else {
+//             // Show filtered notes when a search query is entered
+//             return Column(
+//               children: [
+//                 Expanded(
+//                   child: ListView.builder(
+//                     itemCount: noteController.notes.length,
+//                     itemBuilder: (context, index) {
+//                       final note = noteController.notes[index];
+//                       return InkWell(
+//                         onTap: () async {
+//                           await Navigator.of(context).push(MaterialPageRoute(
+//                               builder: (context) => DescriptionScreen(
+//                                     title: note.title,
+//                                     description: note.description,
+//                                     date: note.date.toString(),
+//                                   )));
+//                           // Add the searched query to recent searches when a note is selected
+//                           searchProvider.addRecentSearch(searchController.text);
+//                         },
+//                         child: Padding(
+//                           padding: const EdgeInsets.symmetric(horizontal: 30),
+//                           child: Card(
+//                             color: Colors.grey.shade800,
+//                             child: Padding(
+//                               padding: const EdgeInsets.all(15.0),
+//                               child: Text(
+//                                 note.title,
+//                                 style: TextStyle(color: Colors.white),
+//                               ),
+//                             ),
+//                           ),
+//                         ),
+//                       );
+//                     },
+//                   ),
 //                 ),
-//               ),
-//             ],
-//           );
+//               ],
+//             );
+//           }
 //         },
 //       ),
 //     );
 //   }
 // }
+
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_app/controller/note_controller/note_controller.dart';
-import 'package:todo_app/model/event_model.dart';
+import 'package:todo_app/controller/search_controller/search_controller.dart';
 import 'package:todo_app/view/description_screen.dart/description_screen.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -86,6 +200,7 @@ class _SearchScreenState extends State<SearchScreen> {
   @override
   Widget build(BuildContext context) {
     var noteController = context.read<NoteController>();
+    var searchProvider = context.read<SearchService>();
 
     return Scaffold(
       backgroundColor: Colors.black,
@@ -104,10 +219,17 @@ class _SearchScreenState extends State<SearchScreen> {
                   setState(() {});
                 }
                 noteController.filterNotes(value);
+                setState(() {});
               },
               decoration: InputDecoration(
                 hintText: "Search for Notes",
-                prefixIcon: Icon(Icons.search),
+                suffixIcon: InkWell(
+                  onTap: () {
+                    noteController.filterNotes(searchController.text);
+                    setState(() {});
+                  },
+                  child: Icon(Icons.search),
+                ),
                 border: OutlineInputBorder(borderSide: BorderSide.none),
               ),
             ),
@@ -119,7 +241,7 @@ class _SearchScreenState extends State<SearchScreen> {
           if (searchController.text.isEmpty) {
             // Show recent searches when the search query is empty
             return FutureBuilder<List<String>>(
-              future: getRecentSearches(),
+              future: searchProvider.getRecentSearches(),
               builder: (context, snapshot) {
                 if (snapshot.connectionState == ConnectionState.waiting) {
                   return CircularProgressIndicator();
@@ -127,12 +249,14 @@ class _SearchScreenState extends State<SearchScreen> {
                   return Text('Error: ${snapshot.error}');
                 } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
                   return Center(
-                    child: Text('No recent searches',
-                        style: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w400,
-                        )),
+                    child: Text(
+                      'No recent searches',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
                   );
                 } else {
                   return Padding(
@@ -159,23 +283,29 @@ class _SearchScreenState extends State<SearchScreen> {
                               return Padding(
                                 padding:
                                     const EdgeInsets.symmetric(horizontal: 8.0),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(
-                                      recentSearch,
-                                      style: TextStyle(
-                                          fontSize: 16, color: Colors.grey),
-                                    ),
-                                    IconButton(
-                                      icon: Icon(Icons.close),
-                                      onPressed: () {
-                                        removeRecentSearch(recentSearch);
-                                        setState(() {});
-                                      },
-                                    ),
-                                  ],
+                                child: InkWell(
+                                  onTap: () {
+                                    searchController.text = recentSearch;
+                                  },
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        recentSearch,
+                                        style: TextStyle(
+                                            fontSize: 16, color: Colors.grey),
+                                      ),
+                                      IconButton(
+                                        icon: Icon(Icons.close),
+                                        onPressed: () async {
+                                          await searchProvider
+                                              .removeRecentSearch(recentSearch);
+                                          setState(() {});
+                                        },
+                                      ),
+                                    ],
+                                  ),
                                 ),
                               );
                             },
@@ -189,13 +319,33 @@ class _SearchScreenState extends State<SearchScreen> {
             );
           } else {
             // Show filtered notes when a search query is entered
+            final filteredNotes = noteController.notes
+                .where((note) => note.title
+                    .toLowerCase()
+                    .contains(searchController.text.toLowerCase()))
+                .toList();
+
+            if (filteredNotes.isEmpty) {
+              // Show "Not Found" message when no search results
+              return Center(
+                child: Text(
+                  'Not Found',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              );
+            }
+
             return Column(
               children: [
                 Expanded(
                   child: ListView.builder(
-                    itemCount: noteController.notes.length,
+                    itemCount: filteredNotes.length,
                     itemBuilder: (context, index) {
-                      final note = noteController.notes[index];
+                      final note = filteredNotes[index];
                       return InkWell(
                         onTap: () async {
                           await Navigator.of(context).push(MaterialPageRoute(
@@ -205,14 +355,18 @@ class _SearchScreenState extends State<SearchScreen> {
                                     date: note.date.toString(),
                                   )));
                           // Add the searched query to recent searches when a note is selected
-                          addRecentSearch(searchController.text);
+                          searchProvider.addRecentSearch(searchController.text);
                         },
-                        child: Card(
-                          child: Padding(
-                            padding: const EdgeInsets.all(15.0),
-                            child: Text(
-                              note.title,
-                              style: TextStyle(color: Colors.white),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 30),
+                          child: Card(
+                            color: Colors.grey.shade800,
+                            child: Padding(
+                              padding: const EdgeInsets.all(15.0),
+                              child: Text(
+                                note.title,
+                                style: TextStyle(color: Colors.white),
+                              ),
                             ),
                           ),
                         ),
@@ -226,26 +380,5 @@ class _SearchScreenState extends State<SearchScreen> {
         },
       ),
     );
-  }
-
-  Future<List<String>> getRecentSearches() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    return prefs.getStringList('recentSearches') ?? [];
-  }
-
-  Future<void> addRecentSearch(String searchQuery) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    List<String> recentSearches = prefs.getStringList('recentSearches') ?? [];
-    recentSearches.insert(0, searchQuery);
-    // Limit the number of recent searches, for example, keep the latest 5 searches
-    recentSearches = recentSearches.take(5).toList();
-    prefs.setStringList('recentSearches', recentSearches);
-  }
-
-  Future<void> removeRecentSearch(String searchQuery) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    List<String> recentSearches = prefs.getStringList('recentSearches') ?? [];
-    recentSearches.remove(searchQuery);
-    prefs.setStringList('recentSearches', recentSearches);
   }
 }
